8992bcb14676e8ac3fdbb7d8bf94cf440b142e30
Ihor Beliuha <60471377+ibeliuha@users.noreply.github.com>
2021-05-23 10:21:08 +0300

    Revert "5th homework done"
    This reverts commit c6649a48f51d8f560cbb9e1d97aaae9f39b24bae.

--- homework_5_airflow/dags/paths.py
+++
@@ -1,9 +0,0 @@
-import os
-import hdfs
-os.chdir('/home/user/airflow/dags')
-
-OUT_OF_STOCK_DATA_PATH = os.path.join(os.getcwd(), 'bronze_layer', 'out_of_stock')
-if not os.path.exists(OUT_OF_STOCK_DATA_PATH):
-    os.mkdir(OUT_OF_STOCK_DATA_PATH)
-CONFIG_PATH = os.path.join(os.getcwd(), 'config.yml')
-LOG_PATH = os.path.join(os.getcwd(), 'log.dat')
